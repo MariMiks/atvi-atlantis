f10685422d7df493b586f4244e886cd0e113c010
Mariana Kuroshima <mariana.izumi.kuroshima.da.silva@ericsson.com>
2024-03-13 21:53:36 -0300

    Adicionando telefones aos clientes

--- teste/index.ts
+++ teste/index.ts
@@ -1,10 +1,23 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
 cliente.dataCadastro = new Date(1840, 6, 23)
 cliente.dataNascimento = new Date(1825, 11, 2)
+
+let telefone1 = new Telefone()
+telefone1.ddd = '012'
+telefone1.numero = '921035634'
+cliente.telefones.push(telefone1)
+
+let telefone2 = new Telefone()
+telefone2.ddd = '012'
+telefone2.numero = '36486271'
+cliente.telefones.push(telefone2)
+
 let endereco = new Endereco()
 endereco.rua = `R. do Catete`
 endereco.bairro = `Copacabana`
@@ -23,5 +36,8 @@ dependente.endereco = (cliente.endereco.clonar() as Endereco)
 dependente.titular = cliente
 cliente.dependentes.push(dependente)
 
+console.log(`\n --------- Lista de Clientes ---------\n`)
 console.log(cliente);
+
+console.log(`\n --------- Lista de Dependentes --------- \n`)
 console.log(dependente);
